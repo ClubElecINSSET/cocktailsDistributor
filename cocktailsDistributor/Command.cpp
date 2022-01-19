c6e7571d719f6cdbf081e17cfab8c22dfbba4ddb
SamuelDeliens <samuel.deliens@gmail.com>
2022-01-19 17:41:46 +0100

    Ajout classe Command

---
+++ cocktailsDistributor/Command.cpp
@@ -0,0 +1,50 @@
+#include "Command.h"
+#include <WString.h>
+
+//Crée une commande
+Command::Command() {
+  
+}
+
+//Crée une commande avec le nom
+Command::Command(String command_) {
+  commandName = command_;
+}
+
+void Command::setName(String name_) {
+  commandName = name_;
+}
+
+//Definis un nouveau parametre avec la position et le parametre
+void Command::setParameter(String parameter, int id) {
+  parametersArray[id] = parameter;
+}
+
+//Retire un parametre à la position souhaitée
+void Command::deleteParameter(int id) {
+  parametersArray[id] = "";
+}
+
+//Retire tous les parametres de la commande
+void Command::clearParameters() {
+  for(int i = 0; i < sizeof(parametersArray)-1; i++){
+    parametersArray[i] = "";
+  }
+}
+
+//Retourne le nom de la commande
+String Command::getName(){
+  return commandName;
+}
+
+//Retourne le parametre de la position souhaitée
+String Command::getParameter(int id){
+  return parametersArray[id];
+}
+
+/*
+//Retourne le tableau des parametres
+String[11] Command::getParameterArray(){
+  return parametersArray;
+}
+*/
