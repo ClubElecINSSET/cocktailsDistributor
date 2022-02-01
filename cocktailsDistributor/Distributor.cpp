6128c32ba2ad4a429d1384d25d945efbb16cd467
Valentin Boulanger <valentin.boulanger@protonmail.com>
2022-02-01 21:37:31 +0100

    Ajout cocktail debug + préparation communication

--- cocktailsDistributor/Distributor.cpp
+++ cocktailsDistributor/Distributor.cpp
@@ -7,13 +7,14 @@
 #include "Liquid.h"
 #include "Command.h"
 #include <Arduino.h>
-#include <SoftwareSerial.h>
 
 Distributor* Distributor::_distributor = NULL;
 
 Distributor::Distributor() {
 	//Marquage de l'objet en cours
 	Distributor::_distributor = this;
+	//Initialisation du port s�rie
+	Serial.begin(9600);
 	//Definition des pompes
 	_pumps[0] = Pump(53);
 	_pumps[1] = Pump(52);
@@ -41,11 +42,19 @@ Distributor::Distributor() {
 	//Parametrage d'une interruption sur le broche de reception des commandes (front montant)
 	attachInterrupt(digitalPinToInterrupt(19), Distributor::handleCommand, RISING);
 	//Creation du service de notification
-	_soundNotifier = SoundNotification(17, 16, 6);
-	_soundNotifier.init();
+	_soundNotifier = SoundNotification(6);
+	// TODO : Envoyer un message d'erreur si le module sonore n'est pas initialisé
+	//if (!_soundNotifier.init()) _communication.sendStatus(STATUS_ERROR);
+	if (!_soundNotifier.init()) Serial.println("ERR");
 	//Creation du service de verification des dosages
 	_cupSensor = CupSensor(14, 15, -219);
 	_cupSensor.init();
+
+	//DEBUG : Création d'un cocktail
+	addLiquid(Liquid(1.0, 1500), 0);
+	Cocktail cock;
+	cock.addInstruction(0, 0, 20);
+	serveCocktail(cock);
 }
 
 void Distributor::handleCommand() {
@@ -103,6 +112,7 @@ void Distributor::executeCommand() {
 bool Distributor::waitForCup() {
 	int cupTimeout = millis() + Configuration::CUP_WAITING_TIMEOUT;
 	//On attend qu'un gobelet soit pose
+	Serial.println("Attente gobelet...");
 	while (!_cupSensor.isCupAvailable()) {
 		if (millis() >= cupTimeout) return false;
 		else if (_cupSensor.isCupRemoved()) {
@@ -127,7 +137,7 @@ void Distributor::serveCocktail(Cocktail cocktail) {
 				if (amount != -1) {
 					int amountPoured = 0;
 					if (Configuration::USE_SOUND_NOTIFICATIONS) _soundNotifier.notifyASync(STEP_POURING);
-					//Remise � z�ro du capteur de gobelet
+					//Remise a zero du capteur de gobelet
 					_cupSensor.tare();
 					//Versement du liquide
 					bool active = true;
@@ -135,13 +145,13 @@ void Distributor::serveCocktail(Cocktail cocktail) {
 					float volumicMass = _pumps[pumpID].getLiquid()->getVolumicMass();
 					while (active) {
 						int measure = _cupSensor.measureLevel();
-						//Tant que le niveau exig� n'est pas atteint, on verse le liquide
+						//Tant que le niveau exige n'est pas atteint, on verse le liquide
 						if (measure < (amount - Configuration::OVERPOURING_AMOUNT) * volumicMass) {
-							//Si le gobelet est retir�, on engage une anomalie
+							//Si le gobelet est retire, on engage une anomalie
 							if (!_cupSensor.isCupRemoved()) {
 								int lastPoured = measure * volumicMass;
 								_pumps[pumpID].activatePump();
-								//Si la quantit� mesur�e est inf�rieure � la quantit� mesur�e juste avant, on engage une anomalie
+								//Si la quantite mesuree est inferieure a la quantite mesuree juste avant, on engage une anomalie
 								if (lastPoured < amountPoured - Configuration::TOLERATED_REGRESSION_MARGIN) {
 									amountPoured += lastPoured;
 									active = false;
@@ -160,20 +170,26 @@ void Distributor::serveCocktail(Cocktail cocktail) {
 						}
 						else active = false;
 					}
-					//Instruction termin�e, d�sactivation de la pompe
+					//Instruction terminee, desactivation de la pompe
 					_pumps[pumpID].deactivatePump();
-					//D�cr�mentation de la quantit� de liquide vers�e
+					//Decrementation de la quantite de liquide versee
 					_pumps[pumpID].getLiquid()->decreaseAmount(amountPoured + Configuration::OVERPOURING_AMOUNT);
-					//Si une anomalie a �t� engag�e, on invite l'utilisateur � retirer son gobelet et on met fin au cocktail
+					//Si une anomalie a ete engagee, on invite l'utilisateur a retirer son gobelet et on met fin au cocktail
 					if (anomaly) {
 						// TODO: Envoi d'un message ANOM
+						//_communication.sendStatus(STATUS_ANOMALY);
 						if (Configuration::USE_SOUND_NOTIFICATIONS) _soundNotifier.notifySync(CUP_IS_REMOVED);
 						break;
 					}
 				}
 			}
 			if (Configuration::USE_SOUND_NOTIFICATIONS) _soundNotifier.notifySync(COCKTAIL_FINISHED);
+			// TODO : Envoyer un message informant que le cocktail est terminé
+			//_communication.sendStatus(STATUS_ENDED);
 		}
+		// TODO : Enregistrer un message d'annulation de cocktail et envoyer un message d'annulation à la tablette
+		else if (Configuration::USE_SOUND_NOTIFICATIONS) _soundNotifier.notifySync(COCKTAIL_FINISHED);
+		//_communication.sendStatus(STATUS_CANCELED);
 	}
     else if (Configuration::USE_SOUND_NOTIFICATIONS) _soundNotifier.notifySync(COCKTAIL_UNAVAILABLE);
 }
